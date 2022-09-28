8777982ed1c121d324c83b12c82d2cf0abfe6444
Nurullah Arslan <codarscoder@gmail.com>
2022-09-28 18:31:46 +0300

    edit

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ line.forEach((item, i) => {
   item.style.transform = `rotate(${i * 10}deg)`;
 });
 
-document.getElementById("deg").addEventListener("change", (e) => {
+document.getElementById("deg").addEventListener("keypress", (e) => {
+  if(e.key == "Enter"){
   aci(e.target.value);
   console.log(e.target.value);
+  }
 });
 
 function aci(deg) {
@@ -20,14 +22,34 @@ function aci(deg) {
   line.forEach((item, i) => {
     if (i * 10 <= deg) {
       console.log(item);
-      item.classList.add("renk");
-    }
-  });
+      if (i*10 < 30) {
+          item.classList.add("renk1");
+      }
+      if (i*10 >= 30 && i*10 < 70 ) {
+          item.classList.add("renk2");
+      }
+      if (i*10 >= 70 && i*10 < 89) {
+          item.classList.add("renk5");
+      }
+      if (i*10 < 135 && i*10 > 89) {
+          item.classList.add("renk3");
+      }
+      if ( i*10 > 135) {
+          item.classList.add("renk4");
+      }
+      }
+      // item.classList.add("renk1");
+    })
+ 
 }
 
 function renkSifirla() {
   line.forEach((item) => {
-    item.classList.remove("renk");
+    item.classList.remove("renk1");
+    item.classList.remove("renk2");
+    item.classList.remove("renk3");
+    item.classList.remove("renk4");
+    item.classList.remove("renk5");
   });
 }
 
