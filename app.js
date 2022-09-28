5cb366e2a01fd840da503e5ba5a74b33223dbbf0
Nurullah Arslan <codarscoder@gmail.com>
2022-09-28 21:56:15 +0300

    edit css

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 let line = document.querySelectorAll(".line");
 let dene = document.querySelector(".dene");
+let monitor = document.querySelector("#monitor");
 
 line.forEach((item, i) => {
   item.style.transform = `rotate(${i * 10}deg)`;
 });
 
 document.getElementById("deg").addEventListener("keypress", (e) => {
-  if(e.key == "Enter"){
-  aci(e.target.value);
-  console.log(e.target.value);
+  if (e.key == "Enter") {
+    aci(e.target.value);
+    console.log(e.target.value);
   }
 });
 
 function aci(deg) {
-  if(deg > 180){
-    deg= 185;
-  }else if(deg < 0){
-    deg= -5;
+  if (deg > 180) {
+    deg = 185;
+  } else if (deg < 0) {
+    deg = -5;
   }
   renkSifirla();
   dene.style.transform = `rotate(0deg)`;
@@ -24,26 +25,37 @@ function aci(deg) {
   line.forEach((item, i) => {
     if (i * 10 <= deg) {
       console.log(item);
-      if (i*10 < 30) {
-          item.classList.add("renk1");
+      if (i * 10 < 30) {
+        item.classList.add("renk1");
       }
-      if (i*10 >= 30 && i*10 < 70 ) {
-          item.classList.add("renk2");
+      if (i * 10 >= 30 && i * 10 < 70) {
+        item.classList.add("renk2");
       }
-      if (i*10 >= 70 && i*10 < 89) {
-          item.classList.add("renk5");
+      if (i * 10 >= 70 && i * 10 < 89) {
+        item.classList.add("renk3");
       }
-      if (i*10 < 135 && i*10 > 89) {
-          item.classList.add("renk3");
+      if (i * 10 < 135 && i * 10 > 89) {
+        item.classList.add("renk3");
       }
-      if ( i*10 > 135) {
-          item.classList.add("renk4");
+      if (i * 10 > 135) {
+        item.classList.add("renk5");
       }
-      }
-      // item.classList.add("renk1");
-    })
-    document.getElementById("deg").value="";
-    document.getElementById("deg").focus();
+    }
+    // item.classList.add("renk1");
+  });
+  let k = -5000;
+  while (k <= deg) {
+    if (k > 180) {
+      monitor.value = "max";
+    } else if (k < 0){
+      monitor.value ="0";
+    }else{
+      monitor.value = ++k;
+    } 
+    k ++;
+  }
+  document.getElementById("deg").value = "";
+  document.getElementById("deg").focus();
 }
 
 function renkSifirla() {
